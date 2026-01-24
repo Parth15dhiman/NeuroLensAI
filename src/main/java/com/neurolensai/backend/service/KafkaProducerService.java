c6c1d049cb299e948d2a9c136d5b583dd5c29d8b
Parth _ <3315.parth.com>
2026-01-24 08:44:21 +0530

    test sending and recieving of image successfully

---
+++ src/main/java/com/neurolensai/backend/service/KafkaProducerService.java
@@ -0,0 +1,27 @@
+package com.neurolensai.backend.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaProducerService {
+
+    @Value("${app.kafka.topic}")
+    private String topicName ;
+    @Autowired
+    private KafkaTemplate<String , byte[] > kafkaTemplate ;
+
+
+    public void sendFrame(byte[] imageBytes){
+        try{
+            kafkaTemplate.send(topicName, imageBytes);
+            log.info("Sent to topic: {}", topicName);
+        } catch (Exception e){
+            log.error("error in sending image bytes", e);
+        }
+    }
+}
