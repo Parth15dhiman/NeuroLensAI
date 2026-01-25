a3a55dff608279726f1ab5a15c09e86ef6d6d5a1
Parth _ <3315.parth@gmail.com>
2026-01-25 15:31:09 +0530

    add controller to fetch frames from extension

---
+++ src/main/java/com/neurolensai/backend/controller/FrameController.java
@@ -0,0 +1,26 @@
+package com.neurolensai.backend.controller;
+
+import com.neurolensai.backend.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/frames")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.POST, RequestMethod.OPTIONS})
+public class FrameController {
+
+    @Autowired
+    private KafkaProducerService producerService ;
+
+    // FrameController.java
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping(value = "/upload", consumes = "multipart/form-data")
+    public ResponseEntity<String> uploadFrame(@RequestParam("file") MultipartFile file) throws IOException {
+        producerService.sendFrame(file.getBytes());
+        return ResponseEntity.ok("Frame Received!");
+    }
+}
