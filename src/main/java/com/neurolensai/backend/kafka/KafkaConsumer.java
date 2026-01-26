41e7d545ea73fe0160190e742f901d31b186c02f
Parth _ <3315.parth@gmail.com>
2026-01-26 17:57:51 +0530

    update kafka consumer for sending detection results to extension(frontend)

--- src/main/java/com/neurolensai/backend/kafka/KafkaConsumer.java
+++ src/main/java/com/neurolensai/backend/kafka/KafkaConsumer.java
@@ -3,13 +3,17 @@ package com.neurolensai.backend.kafka;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.neurolensai.backend.dto.DetectionResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class KafkaConsumer {
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate ;
     private final ObjectMapper objectMapper = new ObjectMapper() ;
 
     @KafkaListener(topics = "detection-results", groupId = "neurolens-backend-group")
@@ -20,6 +24,8 @@ public class KafkaConsumer {
                 "Detection Results from kafka(AI Model) with score :{} and label: {} ",
                     result.getScore(), result.getLabel()
             );
+
+            messagingTemplate.convertAndSend("/topic/detection", result);
         } catch (Exception e){
             log.error("Error parsing AI detection result from kafka: {}", e.getMessage() );
         }
